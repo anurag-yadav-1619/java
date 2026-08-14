f23be62d9c268c9bfc5b739c7a11c64859138bc7
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-08-15 01:55:11 +0530

    Initial Java code

---
+++ basic.java
@@ -0,0 +1,210 @@
+// public class  basic{
+//     public static void main(String[] argS){
+//         System.out.println("anurag yadav ");
+//         System.out.println("anurag yadav ");
+//         System.out.println("anurag yadav");
+//     }
+    
+// }
+
+// public class  basic {
+//     public static void main(String[]args){
+//         System.out.println(67+4);
+
+//     }
+// }
+
+//                                      variables
+
+
+// public class basic{
+//     public static void main(String[] args){
+//         int x=8;
+//         System.out.println(x);
+//         int y=20;
+//         System.out.println("addtition is " +   x+y);
+//         System.out.println(x*y);
+//         System.out.println(x%y);
+//         System.out.println(x/y);
+//         x=x*y; 
+//         System.out.println(x);
+        
+        
+//     }
+// }
+
+// public class basic{
+//     public static void main(String[]agrs){
+//         double x=29,y=90;
+//         System.out.println(x+y);
+//         System.out.println(x*y);
+//         System.out.println(x%y);
+//         System.out.println(x/y);
+
+//     }
+// }
+
+
+
+//                        area of  circle
+
+// public class basic{
+//      public static void main(String[]agrs){
+//         int r=5;
+//         System.out.println(3.14*r*r);
+        
+
+
+//      }
+
+// }
+
+// import java.util.Scanner;
+// public class basic{
+//     public static void main(String[]args){
+//         System.out.print("enter the value : ");
+//         Scanner sc=new Scanner(System.in);
+//         double r=sc.nextDouble();
+//         double a=312*r*r;
+//         System.out.println(a);
+
+
+//     }
+// }
+
+// import java.util.Scanner;
+// public class basic{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("enter the frist number :");
+//         Double a=sc.nextDouble();
+//         System.out.print("enter the srcond number :");
+
+//         Double b=sc.nextDouble();
+//         System.out.print("enter the third number :");
+
+//         Double c=sc.nextDouble();
+//         System.out.println(a+b+c);
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class basic{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("enter the  principal :");
+//         Double p=sc.nextDouble();
+
+//         System.out.print("enter the rete :");
+//         Double r=sc.nextDouble();
+
+//         System.out.print("enter the time :");
+//         Double t=sc.nextDouble();
+
+//         System.out.println((p*r*t)/100);
+//     }
+// }
+
+
+
+//                  modlus opraters
+// = - / %
+
+
+//     public static void main(String[] args){
+//         double a=5/2;
+//         System.out.println(a);
+//     }
+// }
+
+//                        char data  type
+
+// public class basic{
+//     public static void main(String [] args){
+//         char anurag = 's';
+//         System.out.println(anurag);
+
+//          char sujal ='a';
+//          System.out.println(sujal);
+          
+//          char ch='n';
+//          int x=ch;
+//          System.out.println(x);
+
+          
+//          char svnj='A';
+//          int y=svnj;
+//          System.out.println(y);
+//     }
+// } 
+
+
+
+
+// public class basic{
+//     public static void main(String [] args){
+//        int a=10;
+//        System.out.println(a);
+//     //    int b=a+10;
+//        a+=10;
+//        System.out.println(a);
+//         int c=a+10;
+//        System.out.println(c);
+        
+//     }
+// } 
+
+// public class basic{  
+//     public static void main (String [] args){
+//         int x=10;
+//         --x;
+//         System.out.println(x);
+//     }
+// } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
