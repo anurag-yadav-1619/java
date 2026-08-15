c318bb79a50f0aefadafff6d5ebcdf7d2b2bc684
anurag-yadav-1619 <anuragyadava07277@gmail.com>
2026-08-16 01:22:58 +0530

    Initial Java code

---
+++ conditions.java
@@ -0,0 +1,150 @@
+// import java.util.Scanner;
+
+// public class conditions{  
+//     public static void main (String [] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("enter the number :");
+//         int n=sc.nextInt();
+//         if(n%2==0){
+//             System.out.println("it is even number");
+//         }
+//         else {
+//             System.out.println("int is odd");
+//         }
+//     }
+// } 
+
+
+
+// import java.util.Scanner;
+
+// public class conditions{  
+//     public static void main (String [] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("enter the number :");
+//         int n=sc.nextInt();
+//         if(n>=18){
+//             System.out.println("it can vote");
+//         }
+//         else {
+//             System.out.println("it can't vote");
+//         }
+        
+        
+
+
+//     }
+// } 
+  
+// import java.util.Scanner;
+//     public  class conditions{
+//         public static void main(String[] args){
+//             Scanner sc=new Scanner(System.in);
+//             System.out.println("enter the number");
+//             int n=sc.nextInt();
+//             if(n%5==0){
+//                 System.out.println("yes");
+//             }
+//             else{
+//                 System.out.println("no");
+//             }
+//         }
+//     }
+
+
+// import java.util.Scanner;
+//     public  class conditions{
+//         public static void main(String[] args){
+           
+//             Scanner sc=new Scanner(System.in);
+//             System.out.println("enter the number");
+//             int n=sc.nextInt();
+//             if(n>=0){
+//                 System.out.println(n);
+//             }
+//             else{
+//                 System.out.println(-n);
+//             }
+//         }
+//     }
+
+
+
+// import java.util.Scanner;
+//     public  class conditions{
+//         public static void main(String[] args){
+           
+//             Scanner sc=new Scanner(System.in);
+//             System.out.println("enter the number");
+//             int n=sc.nextInt();
+//             if(n<=0) n=-n;
+//             System.out.println(n);
+               
+//         }
+//     }
+
+
+
+//  import java.util.Scanner;
+//  public class conditions{
+//     public static void main(String[] args) {
+//         Scanner cp=new Scanner(System.in);
+//         System.out.println("enter the cost praise");
+//         int c=cp.nextInt();
+//         Scanner sp=new Scanner(System.in);
+//         System.out.println("enter the sellain praise");
+//         int s=sp.nextInt();
+//         if(s>c){
+//             System.out.print("profit ,and theprofit is :" +( s-c));
+//         }
+//         else if (s==c) {
+//             System.out.println("no profit");
+            
+//         }
+//         else{
+//             System.out.print("loss, nad the loss is "+(c-s));
+//         }
+
+//     }
+//  }
+
+
+//  import java.util.Scanner;
+//  public class conditions{
+//     public static void main(String[] args) {
+//         Scanner cp=new Scanner(System.in);
+//         System.out.println("enter the number");
+//         int c=cp.nextInt();
+//         if(c>=1000 && 10000>c){
+//             System.out.print(" yes ,it is four digit number");
+//         }
+       
+//         else{
+//             System.out.print(" no,it is not a four digit number");
+//         }
+
+//     }
+//  }
+
+
+
+//  import java.util.Scanner;
+//  public class conditions{
+//     public static void main(String[] args) {
+//         Scanner cp=new Scanner(System.in);
+//         System.out.println("enter the number");
+//         int c=cp.nextInt();
+//         if(c%5==0 && c%3==0){
+//             System.out.print(" yes ,it is four digit number");
+//         }
+       
+//         else{
+//             System.out.print(" no,it is not a four digit number");
+//         }
+
+//     }
+//  }
+
+
+
+
